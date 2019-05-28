b0528b1816bcc74fcbd626fe61038456788d3d6c
floydezus <oulfc93@gmail.com>
2019-05-28 14:52:52 +0300

    index.js  fix

--- src/index.js
+++ src/index.js
@@ -1,39 +1,30 @@
 import readlineSync from 'readline-sync';
 
-const startGame = (rule, question) => {
-  let name;
-  let step = 1;
-  const numStage = 3;
-
-  const getTitle = () => {
-    console.log('Welcome to the Brain Games!');
-    console.log(rule);
-  };
-  const getPlayerName = () => {
-    name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
-  };
+const startGame = (rule, question) => {  
+  const numStage = 3;  
+  console.log(`Welcome to the Brain Games!\n${rule}`);
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
   const setQuestion = () => {
-    const resFunc = question();
-    console.log(`Question:${resFunc[0]}`);
-    const answer = resFunc[1];
-    const playerAnswer = readlineSync.question('Your answer:');
-    if (playerAnswer === answer) {
-      console.log('Correct!');
-      step += 1;
-      if (step > numStage) {
-        console.log('Congratulations!');
-        return;
+    const iter = (step) => {
+      const resFunc = question();
+      console.log(`Question:${resFunc[0]}`);
+      const answer = resFunc[1];
+      const playerAnswer = readlineSync.question('Your answer:');
+      if (playerAnswer === answer) {
+        console.log('Correct!');
+         if (step === numStage) {
+          console.log('Congratulations!');
+          return;
+        }
+        return iter(step + 1);
+      } else {
+        console.log(`${playerAnswer} is wrong answer ;(. Correct answer was ${answer}.
+        Let's try again, ${name}!`);
       }
-      setQuestion();
-    } else {
-      console.log(`${playerAnswer} is wrong answer ;(. Correct answer was ${answer}.
-      Let's try again, ${name}!`);
-    }
-  };
-
-  getTitle();
-  getPlayerName();
+    };
+    return iter(1);
+  };  
   setQuestion();
 };
 
